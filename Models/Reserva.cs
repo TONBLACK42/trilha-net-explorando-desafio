785be49f1ae663bbb45d4d67f0d57f59c7cbd381
Ailton Alves da Silva <ailton_as@hotmail.com>
2022-10-25 22:49:14 -0300

    Desafio Concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado!
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                //Implementado!
+                throw new Exception($"As suites disponíveis comportam {Suite.Capacidade} hóspede(s). Não é possível hopedar {hospedes.Count}, hóspede(s) no momento.");
             }
         }
 
@@ -35,23 +34,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            //Implementado!
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            //Implementado!
             decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            //Implementado!
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= (10 * valor) / 100;
             }
 
             return valor;
